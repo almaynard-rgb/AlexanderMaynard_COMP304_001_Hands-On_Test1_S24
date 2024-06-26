cfc2f5a92bcdfb000c06d8b486152079e240606b
almaynard-rgb <almaynard2404@gmail.com>
2024-06-26 18:34:42 -0500

    Created initial project and started the AlexanderActivity.kt and activity_main.xml

---
+++ app/src/main/java/com/alexander/maynard/MaynardActivity.kt
@@ -0,0 +1,21 @@
+package com.alexander.maynard
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+// Student ID: 301170707
+class MaynardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_maynard)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
