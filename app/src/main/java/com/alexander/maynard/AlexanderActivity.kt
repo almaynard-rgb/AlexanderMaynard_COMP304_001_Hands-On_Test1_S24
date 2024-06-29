567d83ab6bf6865c603c7195b75921d9c233a699
almaynard-rgb <almaynard2404@gmail.com>
2024-06-28 20:57:49 -0500

    Finished initial work for the Alexander activity (textview, image button and image) and partial implementation for the Maynard activity (checkboxes and button in layout, with partial functional implementation).

--- app/src/main/java/com/alexander/maynard/AlexanderActivity.kt
+++ app/src/main/java/com/alexander/maynard/AlexanderActivity.kt
@@ -1,6 +1,8 @@
 package com.alexander.maynard
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +13,21 @@ class AlexanderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_alexander)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val selectExercisesImgBtn = findViewById<ImageButton>(R.id.activity_alexander_brain_challenge_image_btn)
+        selectExercisesImgBtn.setOnClickListener {
+            val i = Intent(this@AlexanderActivity, MaynardActivity::class.java)
+            startActivity(i)
         }
     }
 }
--- app/src/main/java/com/alexander/maynard/MaynardActivity.kt
+++ app/src/main/java/com/alexander/maynard/MaynardActivity.kt
@@ -1,6 +1,10 @@
 package com.alexander.maynard
 
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +20,29 @@ class MaynardActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val checkboxesArray : Array<CheckBox> = arrayOf(
+            findViewById(R.id.activity_maynard_checkbox_1),
+            findViewById(R.id.activity_maynard_checkbox_2),
+            findViewById(R.id.activity_maynard_checkbox_3),
+            findViewById(R.id.activity_maynard_checkbox_4)
+        )
+
+        val nextBtn = findViewById<Button>(R.id.activity_maynard_next_button)
+        nextBtn.setOnClickListener {
+            var messageToToast : String = ""
+            for (checkbox in checkboxesArray) {
+                if (checkbox.isChecked) {
+                    messageToToast += "${checkbox.text}\n"
+                }
+            }
+            val t = Toast.makeText(applicationContext, messageToToast, Toast.LENGTH_SHORT)
+            t.setGravity(Gravity.FILL, 40, 40)
+            t.show()
         }
     }
 }
