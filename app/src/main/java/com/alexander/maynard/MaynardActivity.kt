fa209fc673c4e63de427d1d33e8d3b2cf66e1a3a
almaynard-rgb <almaynard2404@gmail.com>
2024-07-01 15:29:17 -0500

    Removed the snackbar (not needed).

--- app/src/main/java/com/alexander/maynard/MaynardActivity.kt
+++ app/src/main/java/com/alexander/maynard/MaynardActivity.kt
@@ -13,7 +13,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.snackbar.Snackbar
 
 // Student ID: 301170707
 class MaynardActivity : AppCompatActivity() {
