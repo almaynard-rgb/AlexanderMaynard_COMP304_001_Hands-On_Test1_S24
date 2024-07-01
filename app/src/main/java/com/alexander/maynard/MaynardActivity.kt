a69b3c01825069767aac5848274dfe4aa69d242f
almaynard-rgb <almaynard2404@gmail.com>
2024-06-30 20:12:08 -0500

    Finished functionality to draw multiple lines in the MaynardActivity.kt by using a string-array in strings.xml as defined in the midterm document.

--- app/src/main/java/com/alexander/maynard/MaynardActivity.kt
+++ app/src/main/java/com/alexander/maynard/MaynardActivity.kt
@@ -1,8 +1,13 @@
 package com.alexander.maynard
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -32,6 +37,10 @@ class MaynardActivity : AppCompatActivity() {
             findViewById(R.id.activity_maynard_checkbox_4)
         )
 
+
+        //create zig zag line
+        createZigZagLine(resources.getStringArray(R.array.zig_zag_image_y_coordinates))
+
         val nextBtn = findViewById<Button>(R.id.activity_maynard_next_button)
         nextBtn.setOnClickListener {
             var messageToToast  = ""
@@ -46,6 +55,27 @@ class MaynardActivity : AppCompatActivity() {
 
             //Snackbar to prove everything is there in the Toast as some of the text is cutoff and custom Toasts are deprecated.
             Snackbar.make(it, messageToToast, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun createZigZagLine(yCoordinates: Array<String>) {
+        //for the imageview
+        val zigZagImage = findViewById<ImageView>(R.id.activity_maynard_image_view)
+        val bitmap : Bitmap = Bitmap.createBitmap(800, 100, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.setColor(Color.WHITE)
+        paint.strokeWidth = 8F
+        zigZagImage.setImageBitmap(bitmap)
+        var currentXCoordinate = 0.0F
+
+        for (index in yCoordinates.indices) {
+            if(index == yCoordinates.size -1) {
+                return
+            } else {
+                canvas.drawLine(currentXCoordinate, yCoordinates[index].toFloat(), currentXCoordinate + 100F, yCoordinates[index + 1].toFloat(), paint)
+                currentXCoordinate += 100F
+            }
         }
     }
 }
